35711b48a2b1dfcca0674b4724c774890dcdff20
Sergio Cifuentes <ser.leo.147@gmail.com>
2023-06-17 00:31:25 -0600

    cambio header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <>
         <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>
-            Seguimiento Pacientes {''}
+            Seguimiento de Pacientes {''}
             <span className='text-indigo-600'>Veterinaria</span>
         </h1>      
     </>
